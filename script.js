c6f76576356ebbec4dea404103b144a4613b8416
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-10-25 15:35:08 +0500

    all set and done

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 let nav = document.querySelector('.navMobile');
-
-document.querySelector('.fa-bars').addEventListener('click', ()=>{
-    nav.classList.add('activeNav')
+let increament = document.querySelector('.increament');
+let decreament = document.querySelector('.decreament');
+let numberPrice = document.querySelector('.numberPrice');
+let ethPrice = document.querySelector('.ethPrice');
+document.querySelector('.burger').addEventListener('click', ()=>{
+    nav.classList.add('activeNav');
 
 });
 
 
 document.querySelector('.fa-times').addEventListener('click', ()=>{
-    nav.classList.remove('activeNav')
+    nav.classList.remove('activeNav');
 
 });
+
+let x = 1;
+
+
+increament.addEventListener('click', () =>{
+    
+    numberPrice.innerHTML = ++x;
+    ethPrice.innerHTML = numberPrice.innerHTML;
+})
+
+
+
+decreament.addEventListener('click', () =>{
+if(x>=1){
+    numberPrice.innerHTML = --x;
+    ethPrice.innerHTML = numberPrice.innerHTML;
+}
+
+
+
+else{
+    console.log('asd')
+}
+    
+    
+
+
+    
+})
+
